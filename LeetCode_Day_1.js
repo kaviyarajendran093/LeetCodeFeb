4db16707436c6703858a89dbb074b297dd7634f6
kaviyaRaj <kaviyaraj1193@gmail.com>
2025-03-11 17:15:29 -0400

    remove element using two pointer concept

--- LeetCode_Day_1.js
+++ LeetCode_Day_1.js
@@ -27,4 +27,44 @@ function removeElement(nums, val) {
   return "Total Count = " + count + " ; nums = [" + nums + "]";
 }
 
-console.log(removeElement([3, 2, 2, 3], 3));
+//console.log(removeElement([3, 2, 2, 3], 3));
+
+function removeElementByTwoPointer(nums, val) {
+  let i = 0,
+    j = nums.length - 1;
+  while (j >= i) {
+    if (nums[i] === val) {
+      if (nums[j] === val) {
+        nums[j] = "_";
+        j--;
+      } else {
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+        nums[j] = "_";
+        i++;
+        j--;
+      }
+    } else {
+      i++;
+    }
+  }
+
+  return i + ", nums = [" + nums + "]";
+}
+
+//Leetcode solution
+function removeElement(nums, val) {
+  let index = 0; // Pointer to track position for valid elements
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[index] = nums[i]; // Move valid elements forward
+      index++;
+    }
+  }
+
+  return index; // Returns count of elements not equal to val
+}
+
+console.log(removeElementByTwoPointer([0, 1, 2, 2, 3, 0, 4, 2], 2));
+
+//console.log(removeElementByTwoPointer([3, 2, 2, 3], 3));
